f0449d77c34028aef0e91127a18616393e29f960
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-05 21:51:57 +0600

    phone fetch

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
-const loadAllPhone = async() =>{
+const loadAllPhone = async(status,brandName) =>{
     console.log("All Phone");
     document.getElementById('spinner').style.display='none'
 
-    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${brandName?brandName:'iphone'}`);
     const data =await res.json();
-    displayAllPhone(data.data.slice(0,6))
-
+    if(status){
+        displayAllPhone(data.data)
+    }
+    else{
+        displayAllPhone(data.data.slice(0,6))
+    }
+    
+    console.log(status)
 }
 
 const displayAllPhone =(phones) =>{
     console.log(phones)
 }
 
+const handleShowAll =() =>{
+    loadAllPhone(true)
+}
+
+
 const handleSearch =() =>{
     // console.log('Data');
-    document.getElementById('spinner').style.display='block'
+    document.getElementById('spinner').style.display='block';
+    const searchText =document.getElementById('search-box').value;
+
 
     setTimeout(function (){
-        loadAllPhone();
+        loadAllPhone(false,searchText);
         
     },3000)
 }
