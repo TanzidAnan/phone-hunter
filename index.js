1875a830c705829d29b580cd0354610af5b3b30a
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-05 20:47:05 +0600

    commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const loadAllPhone = () =>{
+const loadAllPhone = async() =>{
     console.log("All Phone");
     document.getElementById('spinner').style.display='none'
+
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
+    const data =await res.json();
+    console.log(data.data)
+
 }
 const handleSearch =() =>{
     // console.log('Data');
