81cafe20fc9641bdba0ae78b079014a0bde8614a
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-09 11:39:12 +0600

    pone display

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const loadAllPhons = async(status,searchText) => {
+const loadAllPhons = async (status, searchText) => {
     console.log(searchText)
     document.getElementById("spinner").style.display = 'none'
     // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
@@ -6,35 +6,57 @@ const loadAllPhons = async(status,searchText) => {
     // .then(data =>console.log(data))
     // .catch((error) =>console.log(error))
 
-    const res =await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText? searchText:'iphone'}`)
-    const data =await res.json();
-    if(status){
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText ? searchText : 'iphone'}`)
+    const data = await res.json();
+    if (status) {
         displayAllPhone(data.data)
     }
-    else{
-        displayAllPhone(data.data.slice(0,6))
+    else {
+        displayAllPhone(data.data.slice(0, 6))
     }
-    
+
 
 }
 
 
-const displayAllPhone=(phones) =>{
-    console.log(phones)
+const displayAllPhone = (phones) => {
+    console.log(phones);
+    const phonesContainer = document.getElementById('phones-container');
+    phones.forEach(phone => {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="card bg-base-100 m-2 w-96 shadow-xl">
+  <figure class="px-10 pt-10">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes"
+      class="rounded-xl" />
+  </figure>
+  <div class="card-body items-center text-center">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+        `;
+        phonesContainer.appendChild(div)
+    });
 }
 
-const handleShowAll=() =>{
-    console.log(5454)
+const handleShowAll = () => {
     loadAllPhons(true)
 }
 
 
 const handleSearch = () => {
     document.getElementById("spinner").style.display = 'block';
-    const searchText =document.getElementById('search-box').value;
+    const searchText = document.getElementById('search-box').value;
     setTimeout(function () {
-        loadAllPhons(false,searchText)
+        loadAllPhons(false, searchText)
     }, 3000)
 }
 
-loadAllPhons(false,'iphone')
+loadAllPhons(false, 'iphone')
