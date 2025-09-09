4fd3c23d518e595df4401bb9d632d141a674847c
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-09 11:12:11 +0600

    searchText

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-const loadAllPhons = () => {
-    console.log('all Phone')
-       document.getElementById("spinner").style.display='none'
+const loadAllPhons = async(status,searchText) => {
+    console.log(searchText)
+    document.getElementById("spinner").style.display = 'none'
+    // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    // .then(res =>res.json())
+    // .then(data =>console.log(data))
+    // .catch((error) =>console.log(error))
+
+    const res =await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText? searchText:'iphone'}`)
+    const data =await res.json();
+    if(status){
+        displayAllPhone(data.data)
+    }
+    else{
+        displayAllPhone(data.data.slice(0,6))
+    }
+    
+
 }
 
+
+const displayAllPhone=(phones) =>{
+    console.log(phones)
+}
+
+const handleShowAll=() =>{
+    console.log(5454)
+    loadAllPhons(true)
+}
+
+
 const handleSearch = () => {
-    document.getElementById("spinner").style.display='block'
-    console.log(6656565);
+    document.getElementById("spinner").style.display = 'block';
+    const searchText =document.getElementById('search-box').value;
     setTimeout(function () {
-        loadAllPhons()
+        loadAllPhons(false,searchText)
     }, 3000)
 }
+
+loadAllPhons(false,'iphone')
